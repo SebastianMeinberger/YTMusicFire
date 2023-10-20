54ef2ffeec69901d19ee549437f0662506e0cac0
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2023-10-20 13:49:40 +0200

    firefox headless is now properly killed and doesnt persist

--- WaitForFfFiles.sh
+++ WaitForFfFiles.sh
@@ -4,3 +4,5 @@ until [ -f $1$2extensions.json ] && [ -f $1$2addonStartup.json.lz4 ]
 do
 	sleep 1
 done
+# Firefox needs to be killed manually, since it persits even after its shell is killed
+kill $(jobs -p) 
